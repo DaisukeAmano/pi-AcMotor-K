9da120e0687897fda59abf1ddb82841acb24dcca
DaisukeAmano <amano.daisuke@astina.co>
2021-10-18 21:28:40 +0900

    edit GPIO for pi and parameters for inverter.

--- pi-AcControl.py
+++ pi-AcControl.py
@@ -5,56 +5,64 @@ import sys
 # LEDとスイッチのGPIO番号
 # デフォルトはRPZ-IR-Sensorの緑LEDと赤SW
 # 必要に応じて変更
-relay_mainIO = 17
-relay_dirIO = 27
-ir_sw = 22
+
+
+inverter_STF = 23
+inverter_STR = 24
+ir_sw = 17
 
 # GPIO番号指定の準備
 GPIO.setmode(GPIO.BCM)
 
 # LEDピンを出力に設定
-GPIO.setup(relay_mainIO, GPIO.OUT)
-GPIO.setup(relay_dirIO, GPIO.OUT)
-
-#待機時間配列とりあえずなし
+GPIO.setup(inverter_STF, GPIO.OUT)
+GPIO.setup(inverter_STR, GPIO.OUT)
 
 # スイッチピンを入力、プルアップに設定
 GPIO.setup(ir_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#物体検知設定・初期化
+objectsNum = 3
 objCount = 0
-dirStatus = False
+dirSTF = True
+dirSTR = False
 
 while True:
     try:
         #Start motor
-        GPIO.output(relay_mainIO, 1)
+        GPIO.output(inverter_STF, dirSTF)
+        GPIO.output(inverter_STR, dirSTR)
         #untill 3 objects detected
-        while objCount<3:
+        while objCount<objectsNum:
             # スイッチの状態を取得
             sw = GPIO.input(ir_sw)
             # 非検出
-            if sw==0:
+            if sw==1:
                 #increment count
                 objCount+=1
                 print(objCount)
-                time.sleep(2)
-                #if objCount<3:
-                    #time.sleep(2)
+                if objCount<objectsNum:
+                    time.sleep(1)
+
             else:
-                GPIO.output(relay_dirIO, dirStatus)
                 time.sleep(0.1)
 
         #Change direction
-        dirStatus = not dirStatus
-        GPIO.output(relay_mainIO, 0)
-        GPIO.output(relay_dirIO, dirStatus)
+        dirSTF = not dirSTF
+        dirSTR = not dirSTR
+        #stop inverter
+        GPIO.output(inverter_STF, False)
+        GPIO.output(inverter_STR, False)
         print('Change direction')
-        print(dirStatus)
+        print('STF', dirSTF, 'STR', dirSTR)
         #reset objCount
         objCount=0
         #decelerating motor
         time.sleep(2)
 
     except KeyboardInterrupt:               #Ctrl+Cキーが押された
+        GPIO.output(inverter_STF, False)
+        GPIO.output(inverter_STR, False)
         GPIO.cleanup()                      #GPIOをクリーンアップ
         print('EXIT')
         sys.exit()
